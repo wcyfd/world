08c1d15d1c4ea27773ca7d74a4797f8821d1642c
aimfd <1101697681@qq.com>
2018-05-20 23:33:02 +0800

    添加匹配专用线程，但是不一定用，先放着

--- src/main/java/org/aimfd/base/ExecutorCenter.java
+++ src/main/java/org/aimfd/base/ExecutorCenter.java
@@ -3,4 +3,5 @@ package org.aimfd.base;
 public class ExecutorCenter {
 	public final static HashExecutor logicExecutor = new HashExecutor("logic", 1);
 	public final static HashExecutor loginExecutor = new HashExecutor("login", 8);
+	public final static HashExecutor matchExecutor = new HashExecutor("match", 1);
 }
