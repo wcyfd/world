97109d3543dd82024b88a6ff5d664820cb5b6849
aimfd <1101697681@qq.com>
2018-05-20 23:36:01 +0800

    添加匹配数据结构

--- src/main/java/org/aimfd/world/system/SystemPeaceData.java
+++ src/main/java/org/aimfd/world/system/SystemPeaceData.java
@@ -1,15 +1,21 @@
 package org.aimfd.world.system;
 
+import org.aimfd.world.system.match.data.IMatchData;
+import org.aimfd.world.system.match.data.MatchData;
 import org.aimfd.world.system.room.data.IRoomData;
 import org.aimfd.world.system.room.data.RoomData;
 
 public class SystemPeaceData extends SystemPeaceDataCodec {
 	public SystemPeaceData() {
 		roomData = new RoomData();
+		matchData = new MatchData();
 	}
 
 	public IRoomData getRoomData() {
 		return roomData;
 	}
 
+	public IMatchData getMatchData() {
+		return matchData;
+	}
 }
--- src/main/java/org/aimfd/world/system/SystemPeaceDataCodec.java
+++ src/main/java/org/aimfd/world/system/SystemPeaceDataCodec.java
@@ -1,11 +1,13 @@
 package org.aimfd.world.system;
 
 import org.aimfd.world.IDataCodec;
+import org.aimfd.world.system.match.data.MatchData;
 import org.aimfd.world.system.room.data.RoomData;
 
 public class SystemPeaceDataCodec implements IDataCodec<String, String> {
 
 	protected RoomData roomData;
+	protected MatchData matchData;
 
 	@Override
 	public String encode() {
